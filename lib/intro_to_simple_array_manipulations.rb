b6eaa38e69cbdebb31ff929fbf0cd11a27590927
Shafee Rushdan <shafeerushdan@yahoo.com>
2021-02-09 22:05:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,6 +2,6 @@ def using_push(array,string)
   array.push(string)
 end 
 
-def using_unshift (array,string)
-  using_unshift.unshift(string)
+def using_unshift (Array,String)
+  using_unshift.unshift(String)
 end 
